79506efacd9a5af1f15ce42b8b3c786ccbc30978
Hung-En Liao <Hung-En Liao>
2023-03-05 15:57:20 -0400

    get put and post

---
+++ booksJson.js
@@ -0,0 +1,59 @@
+//collection Name : books
+// cmd line for insert data : mongosh bookAPI < booksJson.js
+db.books.insertMany([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'War and Peace 2',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }
+  ])
+  
---
+++ models/bookModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const bookModel = new Schema(
+    {
+      title: { type: String },
+      author: { type: String },
+      genre: { type: String },
+      read: { type: Boolean, default: false }
+    }
+
+);
+
+//model(collection, model)
+module.exports = mongoose.model('book', bookModel);
